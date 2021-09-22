4f89b5e4dbb1e7e0d0935f532398ddc6ac2160d7
Douglas Reis <douglasvoreis@gmail.com>
2021-09-21 23:41:23 -0300

    feat: using msql container with node

--- node/main.js
+++ node/main.js
@@ -1,5 +1,21 @@
 const express = require('express');
 
 const app =  express();
-app.listen(3000 , () => console.log('iniciando na porta 3000'));
+const port = 3000;
+
+const dbConfig = {
+    host : 'db' ,
+    user : 'root' ,
+    password : 'root' ,
+    database : 'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(dbConfig);
+
+const sql = `INSERT INTO people (name) VALUES ('douglas')`;
+connection.query(sql);
+connection.end();
+
+app.listen(port , () => console.log(`iniciando na porta ${port}`));
 app.get('/', (req, res) => res.send("Hello world!"));
